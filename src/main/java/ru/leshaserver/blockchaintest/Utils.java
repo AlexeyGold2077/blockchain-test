ec4b8a419633f0b451431e9fa0d0a383478cb8d6
AlexeyGold2077 <alexgold2077@gmail.com>
2024-04-20 15:39:42 +0300

    Added SHA512

--- src/main/java/ru/leshaserver/blockchaintest/Utils.java
+++ src/main/java/ru/leshaserver/blockchaintest/Utils.java
@@ -21,4 +21,21 @@ public class Utils {
 			throw new RuntimeException(e);
 		}
     }
+
+    public static String SHA512(String input) {
+        try {
+			MessageDigest digest = MessageDigest.getInstance("SHA-512");
+			byte[] hash = digest.digest(input.getBytes(StandardCharsets.UTF_8));
+			StringBuilder hexString = new StringBuilder();
+            for (byte b : hash) {
+                String hex = Integer.toHexString(0xff & b);
+                if (hex.length() == 1) hexString.append('0');
+                hexString.append(hex);
+            }
+			return hexString.toString();
+		}
+		catch(Exception e) {
+			throw new RuntimeException(e);
+		}
+    }
 }
