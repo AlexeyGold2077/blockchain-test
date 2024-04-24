1facd4c7b68664ff5b230d9799b5961d76471cf0
AlexeyGold2077 <alexgold2077@gmail.com>
2024-04-24 20:23:18 +0300

    Removed garbage

--- src/main/java/ru/leshaserver/blockchaintest/Controller.java
+++ src/main/java/ru/leshaserver/blockchaintest/Controller.java
@@ -1,11 +1,8 @@
 package ru.leshaserver.blockchaintest;
 
-import java.util.ArrayList;
 import com.google.gson.*;
 
 public class Controller {
-
-    /*public static ArrayList<Block> blockchain = new ArrayList<Block>();*/
 
     public static void main(String[] args) {
         Blockchain blockchain = new Blockchain(4);
